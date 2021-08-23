8ae64472c11856ccf8fac32202cc234266fee5e2
Matheus Andrade <devmatheusandrade@gmail.com>
2021-08-22 21:15:43 -0300

    add exercícios

--- FaseTres/Laboratorio.js
+++ FaseTres/Laboratorio.js
@@ -1,9 +1,12 @@
 /*
 O laboratório fabricante de álcool em gel precisa saber se o produto está atingindo a concentração ideal para desinfecção, que é de 70%. Para a ajudar o laboratório, crie um programa onde será preenchida a capacidade da garrafa em mililitros, e o resultado serão os volumes de álcool e de gel que precisam ser misturados para preenchimento do vasilhame.
 */
-
+const prompt = require("prompt-sync")();
 //Entrada
-
-//Processamento
-
+var num = prompt("Digite a capacidade da garrafa: ");
+var qtdeAlcool = num * 0.70;
+var qtdeGel = num * 0.30;
 //Saída
+console.log(`A capacidade da garrafa é ${num}ml.`);
+console.log(`O volume de álcool é ${qtdeAlcool}ml.`);
+console.log(`O volume de álcool é ${qtdeGel}ml.`);
--- FaseTres/Quadrado.js
+++ FaseTres/Quadrado.js
@@ -3,6 +3,11 @@ Escrever um programa que solicita um número, calcula o dobro do valor, e devolv
 Dica: Utilize uma variável nova para armazenar o resultado.
 O programa “Quadrado” terá um código adaptado do item anterior, para calcular o quadrado do número digitado.
 */
+const prompt = require("prompt-sync")();
 //Entrada
+var num = prompt("Digite um número inteiro: ");
+var result;
 //Processamento
+result = Math.pow(num);
 //Saída
+console.log(`O quadrado de ${num} é ${result}`);
